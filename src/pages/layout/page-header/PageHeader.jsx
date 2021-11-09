6694fd8093f12362ff414b4b5247f7369e5367bf
Roee030 <roee.angel93@gmail.com>
2021-11-09 14:04:44 +0200

    responsive header

--- src/pages/layout/page-header/PageHeader.jsx
+++ src/pages/layout/page-header/PageHeader.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './PageHeader.module.scss';
 import clsx from 'clsx';
 import Button from 'pages/common/button/Button';
 import useBreakpoints from 'utility/hooks/useBreakpoints';
 import logo from 'assest/icons/herolo_logo.png';
+import { useLocation, useHistory } from 'react-router-dom';
+import ROUTES from 'routes';
+import SettingMenu from './SettingMenu';
 
 const PageHeader = () => {
     const { isMobileWidth } = useBreakpoints();
+    const location = useLocation();
+    const history = useHistory();
+
+    const [isSettingMenuOpen, setisSettingMenuOpen] = useState(false);
+
     return (
         <div className={styles.root}>
             <div className={styles.headerRightSide}>
@@ -15,9 +23,43 @@ const PageHeader = () => {
                     : 'HEROLO WEATHER APP'}
             </div>
             <div className={styles.headerLeftSide}>
-                <Button className={styles.button}>HOME</Button>
-                <Button className={styles.button}>FAVORITE</Button>
-                <Button className={styles.button}>SETTINGS</Button>
+                <Button
+                    className={clsx(styles.button,
+                        { [styles.buttonClicked]: location.pathname === ROUTES.HOME }
+                    )}
+                    onClick={() => {
+                        history.push({
+                            pathname: ROUTES.HOME,
+
+                        });
+                    }}
+                >HOME</Button>
+                <Button
+                    className={clsx(styles.button,
+                        { [styles.buttonClicked]: location.pathname === ROUTES.FAVORITE }
+                    )}
+                    onClick={() => {
+                        history.push({
+                            pathname: ROUTES.FAVORITE,
+
+                        });
+                    }}
+                >FAVORITE</Button>
+                <div>
+
+                    <Button className={clsx(styles.button,
+                        { [styles.buttonClicked]: isSettingMenuOpen }
+                    )}
+                        onClick={() => {
+                            setisSettingMenuOpen(!isSettingMenuOpen);
+                        }}
+                    >
+
+                        SETTINGS
+
+                    </Button>
+                    {isSettingMenuOpen && <SettingMenu />}
+                </div>
             </div >
         </div >
     );
